5fd05d3db118dae177b546625fa0b87919ee3735
Pedro Lucas Kawano <pedrolucas_kawano@hotmail.com>
2023-09-26 21:26:41 -0300

    added new entities and methods.

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -2,10 +2,36 @@ namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? ExpirateDate { get; set; }
-        public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expirateDate)
+        {
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            ExpirateDate = expirateDate;
+            Active = true;
+            _payments = new List<Payment>();
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpirateDate { get; private set; }
+        public bool Active { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get; private set; }
+
+        public void AddPayment(Payment payment)
+        {
+            _payments.Add(payment);
+        }
+
+        public void Activate()
+        {
+            Active = true;
+            LastUpdateDate = DateTime.Now;
+        }
+        public void Inactivate()
+        {
+            Active = false;
+            LastUpdateDate = DateTime.Now;
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("André", "Baltieri", "12345678901", "hello@balta.io");
+        student.AddSubscription(subscription);
+    }
+}
